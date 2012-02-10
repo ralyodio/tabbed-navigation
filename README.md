5cf3cc4dea93d81d9dfbbcc95508306c5d732f98
Anthony Ettinger <anthony@ettinger.name>
2012-02-10 10:38:32 -0800

    initial plugin

---
+++ README.md
@@ -0,0 +1,33 @@
+tabbed-navigation
+===========
+
+tabbed-navigation is light-weight jQuery plugin for handling tabbed navigation with minimal markup.
+
+Demo
+----
+
+http://chovy.dyndns.org/jquery/tabbed-navigation/
+
+Usage
+-----
+
+	<section id="example">
+		<div class="tabbed">
+			<ul class="tabs">
+				<li><a href="#foo">Foo</a></li>
+				<li><a href="#bar">Bar</a></li>
+				<li><a href="#baz">Baz</a></li>
+			</ul>
+			<div class="contents">
+				<div class="content" id="foo">Foo content</div>
+				<div class="content" id="bar">Bar content</div>
+				<div class="content" id="baz">Baz content</div>
+			</div>
+		</div>
+	</section>
+
+
+Requirements
+------------
+
+jQuery (tested with 1.7.1) 
---
+++ tabbed.js
@@ -0,0 +1,33 @@
+(function($){
+	$.fn.extend({ 
+		tabbed: function() {
+
+			return this.each(function() {
+				var obj = $(this);
+
+				var tabs = $('.tabs li', obj),
+				tabContents = $('.contents .content', obj);
+
+				//make the first tab active
+				tabs.removeClass('active');
+				tabs.eq(0).addClass("active");
+
+				//hide all but the first tab contents
+				tabContents.hide();
+				tabContents.eq(0).show();
+
+				tabs.click(function() {
+
+					//Make new tab active
+					tabs.removeClass("active");
+					$(this).addClass("active");
+
+					//Show new tab content
+					tabContents.hide();
+					var current = tabs.index($(this));
+					tabContents.eq(current).show();
+				});
+			});
+		}
+	});
+})(jQuery);
